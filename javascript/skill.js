4297e0c7a95e46bb7d7b6d4dae4ba1b6252fa714
rishabh01solanki <rishabh01solanki@gmail.com>
2023-01-06 13:50:51 -0600

    fixed skillbar animation

--- javascript/skill.js
+++ javascript/skill.js
@@ -1,3 +1,9 @@
-
-
+  document.addEventListener("DOMContentLoaded", function() {
+    // Select all elements with class "skill-percent"
+    var skillBars = document.querySelectorAll(".skill-percent");
+    // Add the "animate-bar" class to each element
+    skillBars.forEach(function(skillBar) {
+      skillBar.classList.add("animate-bar");
+    });
+  });
   
